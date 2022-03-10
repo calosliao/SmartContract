2e3298ae39e7c3c5a5851eddde3debb526299652
calosliao <49058187+calosliao@users.noreply.github.com>
2022-03-10 18:48:51 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ files.sort()
 m = 0
 for file in files:
     print(file)
-    os.system("python oyente.py -s contract/{i}.sol 2>&1 | tee result_oyente/{k}.log".format(i=m, k=file))
+    os.system("python oyente.py -s smart/{i}.sol 2>&1 | tee result_oyente/{k}.log".format(i=m, k=file))
     m += 1
