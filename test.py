7fd1ac4b4a14320204b193adc470c5fc9ad2dc31
calosliao <49058187+calosliao@users.noreply.github.com>
2022-03-10 18:33:18 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import os
 
-path = "contract"
+path = "smart"
 files= os.listdir(path)
 files.sort()
 m = 0
 for file in files:
     print(file)
-    os.system("python oyente.py -s contract/{i}.sol 2>&1 | tee result/{i}.log".format(i=m))
+    os.system("python oyente.py -s contract/{i}.sol 2>&1 | tee result_oyente/{i}.log".format(i=m))
     m += 1
